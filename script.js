d9ccd06f5679167b844dd21ef0ed966c9246fe45
Michael Jonathan Amay <michaeljonathanamay@lpc-depaulsecure-243-90.depaulsecure-student.depaul.edu>
2025-12-05 15:29:08 -0600

    refactoring

--- script.js
+++ script.js
@@ -4,41 +4,46 @@ sendApiRequest()
 async function sendApiRequest(){
 
   let todayDate = new Date();
-
   let modifieddate = "";
-
   modifieddate += todayDate.getFullYear() + "-" + todayDate.getMonth() + "-" + todayDate.getDate();
 
+  const temp = `https://api.nasa.gov/planetary/apod?api_key=D52GrrGbtuaqFg0YE8YQkvOPKHRhfaX219kA1Qih&date=`+modifieddate;
+
+  console.log("temp: " + temp);
+
   //note API key; must renew every so often
-  let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=D52GrrGbtuaqFg0YE8YQkvOPKHRhfaX219kA1Qih&date=`+modifieddate);
+  let response = await fetch(temp);
+
+  console.log("response: " + response)
 
   let data = await response.json();
+
+  console.log("data: " + data)
 
   //loop over json object
   for(const key in data){
 
-    //console.log(`${key}: ${data[key]}`)
+      //console.log(`${key}: ${data[key]}`)
 
-    const url = data[key];
-
-    if(key == "url"){
       const url = data[key];
 
+      if(key == "url"){
 
-      pictureShow(url);
+        const url = data[key];
 
-      //console.log(url);
-    }
-    
+        console.log("url: "+ url);
+
+        pictureShow(url);
+
+      }
   }
-
 }
 
 function pictureShow(linkData){
 
   var imageData = "<div><img src=" + linkData + "></div>";
 
-  console.log(imageData);
+  console.log("imageData: "+ imageData);
 
   document.querySelector(".container").innerHTML = imageData;
 
